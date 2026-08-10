7fe5a3352e4805d278e3eb45d03a195ad21ce9f5
#Patriot0911 <alexandr.potaskalov@gmail.com>
2026-08-10 11:21:35 +0300

    feat: add better .env.example

--- server/src/main.ts
+++ server/src/main.ts
@@ -32,7 +32,7 @@ async function bootstrap() {
     SwaggerModule.createDocument(app, swaggerConfig),
   );
 
-  const port = process.env.PORT ?? 3000;
+  const port = process.env.SERVER_PORT ?? 4000;
   await app.listen(port);
   console.log(`Server running on http://localhost:${port}`);
   console.log(`Swagger docs at http://localhost:${port}/api/docs`);
